2bd6283d919b8c522052b87701572fbb9058ad01
Jeff Hobson <jeffrey296@gmail.com>
2022-02-18 13:26:25 -0500

    Add livesplit replicant and display info in panel

--- extension/index.js
+++ extension/index.js
@@ -2,24 +2,30 @@
 const LiveSplitClient = require('livesplit-client')
 
 module.exports = nodecg => {
-	nodecg.log.info('Hello, from your bundle\'s extension!'); // Semi-colon needed here because of self-calling function
-	(async () => {
+	const rLivesplit = nodecg.Replicant('livesplit')
+	
+	;(async () => {
 		try {
 			const client = new LiveSplitClient(`${nodecg.bundleConfig.url}:${nodecg.bundleConfig.port}`)
 
 			client.on('connected', () => {
 				nodecg.log.info('Connected to ls')
+				rLivesplit.value.connection.status = 'connected'
 			})
 
 			client.on('disconnected', () => {
 				nodecg.log.info('Disconnected from ls')
+				rLivesplit.value.connection.status = 'disconnected'
 			})
 			
 			await client.connect()
 			const info = await client.getAll()
 			nodecg.log.info('Summary:', info)
+			rLivesplit.value.timer = info
 
-			client.disconnect()
+			setInterval(async () => {
+				rLivesplit.value.timer = await client.getAll()
+			}, 1000)
 		} catch (err) {
 			nodecg.log.info(err)
 		}
